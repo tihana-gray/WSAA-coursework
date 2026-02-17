e1c95a72910c99b7d47b800c182344294eea4132
Tihana Gray <tihanagray64@gmail.com>
2026-02-17 17:10:28 +0000

    assignment 2 - finished work, added pirate talk to make it funny.

--- Assignments/assignment2-carddraw.py
+++ Assignments/assignment2-carddraw.py
@@ -1,4 +1,4 @@
-# Program that prints out 5 cards
+# Program that prints out 5 cards and talks like a pirate.
 # Author: Tihana Gray
 
 import requests
@@ -25,10 +25,6 @@ data = response.json()
 # .json() converts the JSON response into a Python dictionary.
 # 📚 Reference: https://docs.python-requests.org/en/latest/user/quickstart/#json-response-content
 
-print(data)
-# Checking the structure of the data returned by the API.
-# Printed response: {'success': True, 'deck_id': '4t3z4u6f5c92', 'remaining': 52, 'shuffled': True}
-# deck_id is a key that we will use to draw cards from the deck.
 
 deck_id = data["deck_id"]
 # Accessing the value associated with the key 'deck_id' from the data dictionary.
@@ -53,9 +49,6 @@ draw_data = response.json()
 # JSON must be converted into native Python data structures before it can be accessed.
 # 📚 Reference: https://docs.python-requests.org/en/latest/user/quickstart/#json-response-content
 
-print(draw_data)
-# Outputs the entire draw response dictionary to the console and checks its structure.
-
 cards = draw_data["cards"]
 # Here we access the 'cards' in the 'draw_data' dictionary.
 # It stores the associated list of card dictionairies in the variable 'cards'.
@@ -66,17 +59,17 @@ for card in cards:
 # Each 'card' is a dictionary containing details about the card.
 # 📚 Reference: https://docs.python.org/3/tutorial/controlflow.html#for-statements
     value = card["value"]
-# Extracts the value field from the current card dictionary.
-# Stores the value in the variable 'value'.
-# 📚 Reference: https://docs.python.org/3/tutorial/datastructures.html#dictionaries
+    # Extracts the value field from the current card dictionary.
+    # Stores the value in the variable 'value'.
+    # 📚 Reference: https://docs.python.org/3/tutorial/datastructures.html#dictionaries
 
-suit = card["suit"]
-# Accesses the value associated with the key 'suit' in the card dictionary.
-# The value represents the card's suit and is a string.
+    suit = card["suit"]
+    # Accesses the value associated with the key 'suit' in the card dictionary.
+    # The value represents the card's suit and is a string.
 
-print(value, "of", suit)
-# Involves 3 separate arguments: the value of the card, the string "of", and the suit of the card.
-# 📚 Reference: https://stackoverflow.com/questions/49682992/python-printing-from-a-list-that-contains-values-from-a-class
+    print(value, "of", suit)
+    # Involves 3 separate arguments: the value of the card, the string "of", and the suit of the card.
+    # 📚 Reference: https://stackoverflow.com/questions/49682992/python-printing-from-a-list-that-contains-values-from-a-class
 
 values = []
 suits = []
@@ -113,6 +106,68 @@ counts = value_counts.values()
 # This line retrieves the counts of each card value from the value_counts dictionary.
 # 📚 Reference: https://docs.python.org/3/library/stdtypes.html#dict.values
 
+if 3 in counts: # This line checks if there are 3 cards of the same value in the hand.
+    print("Ahoy, pirate! Three o' the same, the sea favors ye today!")
+elif 2 in counts: # This line checks if there are 2 cards of the same value in the hand.
+    print("Shiver me timbers! A pair be in yer grasp! A grand haul, indeed.")
+# 📚 References: https://docs.python.org/3/tutorial/controlflow.html#if-statements
+# https://docs.python.org/3/reference/expressions.html#membership-test-operations
+# https://www.w3schools.com/python/gloss_python_elif.asp
 
+if len(set(suits)) == 1: # This line checks if all the suits in the hand are the same.
+    print("By the beard of Blackbeard, ye’ve struck gold! All cards be of the same suit! A flush, ye say?")
+# 📚 References: https://docs.python.org/3/tutorial/datastructures.html#sets
+# https://docs.python.org/3/library/functions.html#len
+# https://realpython.com/len-python-function/
 
+value_map = {
+    "ACE": 14,
+    "KING": 13,
+    "QUEEN": 12,
+    "JACK": 11
+}
+# This part creates a dictionary that maps the face card values to their corresponding numerical values.
+# Straight detection requires numerical values to compare the cards.
+# 📚 Reference: https://docs.python.org/3/tutorial/datastructures.html#dictionaries
 
+numeric_values = []
+# This creates an empty list to store the numeric values of the cards.  
+# 📚 Reference: https://docs.python.org/3/tutorial/datastructures.html#more-on-lists
+
+for value in values:
+    if value in value_map:
+        numeric_values.append(value_map[value]) # If the value is a face card, append its mapped numeric value.
+    else:
+        numeric_values.append(int(value)) # If it's a number card, convert it to an integer and append it.
+# 📚 References: https://www.geeksforgeeks.org/python/python-if-else/
+# https://www.geeksforgeeks.org/python/python-list-append-method/
+# https://realpython.com/python-append/
+# https://docs.python.org/3/tutorial/datastructures.html#more-on-lists
+# https://docs.python.org/3/library/functions.html#int
+
+numeric_values.sort() # Sorts the list in ascending order to check for straights.
+# 📚 Reference: https://docs.python.org/3/tutorial/datastructures.html#more-on-lists
+
+is_straight = True # This is the initial assumption that the hand is a straight until proven otherwise.
+for i in range(1, len(numeric_values)): # This loop iterates through the sorted numeric values starting from the second card.
+    if numeric_values[i] != numeric_values[i-1] + 1: # This condition checks if the current card value is not exactly 1 greater than the previous card value.
+        is_straight = False # If the condition is true, it means the cards are not in a sequence, so we set is_straight to False.
+        break # Exits the loop early since we have already determined that the hand is not a straight.
+# 📚 References: https://docs.python.org/3/library/functions.html#func-range
+# https://docs.python.org/3/library/functions.html#len
+# https://docs.python.org/3/reference/expressions.html#comparisons
+# https://docs.python.org/3/reference/simple_stmts.html#the-break-statement
+
+if is_straight: # If the is_straight variable is still True after the loop, it means all cards are in a sequence.
+    print ("Luck be sailin’ with ye! A straight be in yer hand, the winds be in yer favor!")
+    
+if not (3 in counts or 
+        2 in counts or 
+        len(set(suits)) == 1 or 
+        is_straight):
+    print("Deal again and test yer fate!")
+# This condition checks if none of the winning conditions are met (no three of a kind, no pair, no flush, no straight).
+# If none of these conditions are true, it prints a message encouraging the player to try again.
+# 📚 References: https://docs.python.org/3/tutorial/controlflow.html#if-statements  
+# https://docs.python.org/3/reference/expressions.html#membership-test-operations
+# https://docs.python.org/3/library/functions.html#len  
